f5bc70eabf63e91987ed18efa7cb007e686b899b
H0onnn <dukei2017@naver.com>
2023-05-29 15:20:22 +0900

    더미 데이터로 폼 사용해보기

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "48082e040c079b13"; }
+/******/ 		__webpack_require__.h = function() { return "398b931fa3c180f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,16 @@
 // our-domain.com/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetUpPage() {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+
+  return (
+    <>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
+}
+
+export default NewMeetUpPage;
